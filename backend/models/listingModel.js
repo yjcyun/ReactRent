08b99597e142ede76c65a25b56c01a5a8c47f39f
Christina Yujeong Yun <yjc.yun@gmail.com>
2020-09-04 18:53:36 -0400

    Write listing model

---
+++ backend/models/listingModel.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+const slugify = require('slugify');
+
+const listingSchema = new mongoose.Schema({
+  address: {
+    type: String,
+    required: [true, 'This is a required field'],
+    trim: true
+  },
+  city: {
+    type: String,
+    required: [true, 'This is a required field'],
+    trim: true
+  },
+  sqft: {
+    type: Number,
+    required: [true, 'This is a required field'],
+    min: [1, 'This field must be greater than 1']
+  },
+  garage: {
+    type: Number,
+    required: [true, 'This is a required field']
+  },
+  bedrooms: {
+    type: Number,
+    required: [true, 'This is a required field']
+  },
+  bathrooms: {
+    type: Number,
+    required: [true, 'This is a required field']
+  },
+  price: {
+    type: Number,
+    required: [true, 'This is a required field'],
+    min: [1, 'Price must be greater than 1']
+  },
+  postedDate: {
+    type: Date,
+    default: Date.now(),
+    select: false
+  },
+  description: {
+    type: String
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'Image cover is required']
+  },
+  images: [String]
+});
+
+// Slugify based on address
+listingSchema.pre('save', function (next) {
+  this.slug = slugify(this.address, { lower: true });
+  next();
+});
+
+const Listing = mongoose.model('Listing', listingSchema);
+
+module.exports = Listing;
+
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,19 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 dotenv.config({ path: './config.env' });
 
 const app = require("./app");
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(()=> console.log('DB successfully connected'));
 
 const port = process.env.PORT || 5001;
 app.listen(port, () => console.log(`App listening on port ${port}`));
